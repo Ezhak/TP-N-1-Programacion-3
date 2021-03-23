b13b8ceff36b4dbb02f3fec6efa5569e7899b2f8
ipetrignani <73859437+ipetrignani@users.noreply.github.com>
2021-03-23 15:17:12 -0300

    Add frmEj1 design

--- TP Nº1 Programacion 3/frmEj1.Designer.cs
+++ TP Nº1 Programacion 3/frmEj1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace TP_Nº1_Programacion_3
+﻿namespace TP_Nº1_Programacion_3
 {
     partial class frmEj1
     {
@@ -29,21 +28,104 @@ namespace TP_Nº1_Programacion_3
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnSingular = new System.Windows.Forms.Button();
+            this.btnMultiple = new System.Windows.Forms.Button();
+            this.lstBox1 = new System.Windows.Forms.ListBox();
+            this.lstBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(27, 24);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(100, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Ingrese un Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(133, 21);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(256, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(395, 18);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnSingular
+            // 
+            this.btnSingular.Location = new System.Drawing.Point(223, 123);
+            this.btnSingular.Name = "btnSingular";
+            this.btnSingular.Size = new System.Drawing.Size(41, 23);
+            this.btnSingular.TabIndex = 3;
+            this.btnSingular.Text = ">";
+            this.btnSingular.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiple
+            // 
+            this.btnMultiple.Location = new System.Drawing.Point(223, 242);
+            this.btnMultiple.Name = "btnMultiple";
+            this.btnMultiple.Size = new System.Drawing.Size(41, 23);
+            this.btnMultiple.TabIndex = 4;
+            this.btnMultiple.Text = ">>";
+            this.btnMultiple.UseVisualStyleBackColor = true;
+            // 
+            // lstBox1
+            // 
+            this.lstBox1.FormattingEnabled = true;
+            this.lstBox1.Location = new System.Drawing.Point(30, 84);
+            this.lstBox1.Name = "lstBox1";
+            this.lstBox1.Size = new System.Drawing.Size(180, 212);
+            this.lstBox1.TabIndex = 5;
+            // 
+            // lstBox2
+            // 
+            this.lstBox2.FormattingEnabled = true;
+            this.lstBox2.Location = new System.Drawing.Point(283, 84);
+            this.lstBox2.Name = "lstBox2";
+            this.lstBox2.Size = new System.Drawing.Size(187, 212);
+            this.lstBox2.TabIndex = 6;
             // 
             // frmEj1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 487);
+            this.ClientSize = new System.Drawing.Size(505, 330);
+            this.Controls.Add(this.lstBox2);
+            this.Controls.Add(this.lstBox1);
+            this.Controls.Add(this.btnMultiple);
+            this.Controls.Add(this.btnSingular);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmEj1";
-            this.Text = "Form2";
+            this.Text = "Ejercicio 1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmEj1_FormClosing);
             this.Load += new System.EventHandler(this.frmEj1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSingular;
+        private System.Windows.Forms.Button btnMultiple;
+        private System.Windows.Forms.ListBox lstBox1;
+        private System.Windows.Forms.ListBox lstBox2;
     }
 }
